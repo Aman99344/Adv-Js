cb4e49159ba5ecca50fc2df082e9201d62a3b095
Aman Gupta <amangupta99344@gmail.com>
2023-01-15 23:57:12 +0530

    countries data visulation

---
+++ assignment/405-01/index.js
@@ -0,0 +1,14 @@
+const subheading = document.getElementById("subheading")
+// console.log(subheading);
+subheading.innerText = `currently, we have ${countries_data.length} countries`
+
+const graphTitle = document.getElementById("graphTitle")
+
+
+function buttonOneClicked(){
+   console.log("one");
+}
+
+function buttonTwoClicked(){
+ console.log("two");
+}
