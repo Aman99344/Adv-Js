03b9636b25f8734881ea75a289b6e87586fbd686
Aman Gupta <amangupta99344@gmail.com>
2023-01-16 13:25:38 +0530

    countries data  visualization

--- assignment/405-01/index.js
+++ assignment/405-01/index.js
@@ -1,55 +1,62 @@
-const subheading = document.getElementById("subheading");
-// console.log(subheading);
-subheading.innerText = `currently, we have ${countries_data.length} countries`;
+const subHeading = document.getElementById("subheading")
+subHeading.innerText = `Currently we have ${countries_data.length} countries`
 
-const graphTitle = document.getElementById("graphTitle");
+const graphTitle = document.getElementById("graphTitle")
 
 const sortedData = countries_data.sort(function (a, b) {
-  return b.population - a.population;
-});
+    return b.population - a.population
+})
 
-let totalPopulation = 0;
-
-for(let i =0; i<countries_data.length; i++){
+let totalPopulation = 0
+for(let i=0; i<countries_data.length; i++){
     totalPopulation += countries_data[i].population
 }
 
-console.log(totalPopulation);
-
+const clear = document.getElementById("graph")
+// clear.style.border = "2px solid red"
 function buttonOneClicked() {
-  console.log("one");
-  graphTitle.innerText = "10 most populated contries in the world";
+    clear.innerHTML = " "
+    graphTitle.innerText = "10 most populated countries in world"
 
-  for (let i = 0; i < 10; i++) {
-    const oneDiv = document.createElement("div");
-    
-    const country_Name = document.createElement("p");
-    country_Name.innerText = sortedData[i].name;
-    
-    oneDiv.appendChild(country_Name);
-    
-    // const percentDiv = document.createElement("div")
-    const percentageDiv = document.createElement("div")
-    
-    const percentage = (sortedData[i].population/totalPopulation)*100;
-    percentageDiv.style.width = `${percentage}%`
-    percentageDiv.style.backgroundColor = "orange"
+    for(let i=0; i<10; i++){
+        const oneDiv = document.createElement("div")
+        oneDiv.style.display = "flex"
+        oneDiv.style.height = "6.5vh"
+        oneDiv.style.alignItems = "center"
+        oneDiv.style.justifyContent = "center"
+        // oneDiv.style.border = "2px solid green"
 
-    percentageDiv.innerText = percentage;
-
-    const graphDiv = document.getElementById("graph")
-    oneDiv.appendChild(percentageDiv)
-    graphDiv.appendChild(oneDiv)
-
-    console.log(percentage);
-    console.log(oneDiv);
-  }
-} 
+        const countryName = document.createElement("p")
+        countryName.innerText = sortedData[i].name
+        countryName.style.width = "20%"
+        oneDiv.appendChild(countryName)
 
 
+        const percentDiv = document.createElement("div")
+        const percentageDiv = document.createElement("div")
+        const percentage = (sortedData[i].population/totalPopulation)*100
+        percentageDiv.style.width = `${percentage}%`
+        percentageDiv.style.backgroundColor = "orange"
+        percentageDiv.style.height = "fit-content"
+        
+        percentageDiv.innerText = `${percentage.toFixed(2)}%`
 
-function buttonTwoClicked() {
-  console.log("two");
-  graphTitle.innerText = "10 most spoken language in the world";
+        percentDiv.appendChild(percentageDiv)
+        percentDiv.style.width = "30%"
+        oneDiv.appendChild(percentDiv)
+        
+        const populationDiv = document.createElement("div")
+        populationDiv.innerText = sortedData[i].population
+        oneDiv.appendChild(populationDiv)
+        
+        const graphDiv = document.getElementById("graph")
+        graphDiv.appendChild(oneDiv)
+    }
 }
 
+function buttonTwoClicked() {
+    clear.innerHTML = " "
+
+    graphTitle.innerText = "10 most spoken languages in world"
+
+}
